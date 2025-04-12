57fa7c9ac027150ea21d09c69037dda878350a5b
lolalex1 <alex.atanasovski03@gmail.com>
2025-04-12 05:20:31 +0000

    REwrite for 14 and 15

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,25 @@
 #include "binary_trees.h"
 
 /**
+ * height - measure height of a subtree
+ * @tree: pointer to node
+ *
+ * Return: height or 0 if tree is NULL
+ */
+static int height(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = height(tree->left);
+	right = height(tree->right);
+
+	return (1 + (left > right ? left : right));
+}
+
+/**
  * binary_tree_balance - measure balance factor of binary tree
  * @tree: pointer to root node
  *
@@ -14,9 +33,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		left = (int)binary_tree_height(tree->left);
+		left = height(tree->left);
 	if (tree->right)
-		right = (int)binary_tree_height(tree->right);
+		right = height(tree->right);
 
 	return (left - right);
 }
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -4,15 +4,17 @@
  * binary_tree_is_full - check if binary tree is full
  * @tree: pointer to root node
  *
- * Return: 1 if full, 0 if not or if tree is NULL
+ * Return: 1 if full, 0 otherwise or if tree is NULL
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->left == NULL && tree->right == NULL) ||
-	    (tree->left && tree->right))
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
 	return (0);
